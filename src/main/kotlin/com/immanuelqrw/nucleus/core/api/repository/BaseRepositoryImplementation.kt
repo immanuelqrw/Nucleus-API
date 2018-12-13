4d1ec813849331d22a2bd1d90e6fe4eb93926e6d
Immanuel Washington <code.immanuelqrw@gmail.com>
2018-12-12 22:09:03 -0500

    Converted Repository into interface
    Spring prefers Repositories to be labelled as interfaces
    Created BaseRepositoryImplementation for inheriting from class

--- src/main/kotlin/com/immanuelqrw/nucleus/core/api/repository/BaseRepositoryImplementation.kt
+++ src/main/kotlin/com/immanuelqrw/nucleus/core/api/repository/BaseRepositoryImplementation.kt
@@ -1,14 +1,13 @@
 package com.immanuelqrw.nucleus.core.api.repository
 
 import com.immanuelqrw.nucleus.core.api.model.BaseEntity
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation
 import org.springframework.data.jpa.repository.support.SimpleJpaRepository
 import javax.persistence.EntityManager
 
 /**
- * Base Repository interface
+ * Base Repository implementatio class
  *
  * @property T Entity type being manipulated
  */
-interface BaseRepository<T : BaseEntity>: JpaRepositoryImplementation<T, Long>
+abstract class BaseRepositoryImplementation<T : BaseEntity>(domainClass: Class<T>, entityManager: EntityManager)
+    : SimpleJpaRepository<T, Long>(domainClass, entityManager), BaseRepository<T>
