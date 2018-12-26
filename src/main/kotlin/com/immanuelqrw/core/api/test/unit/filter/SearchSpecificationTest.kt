7432fa6b15b8ccf109ca76c181df0e648ad78db5
Immanuel Washington <code.immanuelqrw@gmail.com>
2018-12-26 03:18:18 -0500

    SearchSpecification test
    Added unit tests for SearchSpecification
    
    Test intended to expect thrown exception currently failing

--- src/main/kotlin/com/immanuelqrw/core/api/test/unit/filter/SearchSpecificationTest.kt
+++ src/main/kotlin/com/immanuelqrw/core/api/test/unit/filter/SearchSpecificationTest.kt
@@ -1,21 +1,39 @@
-package com.immanuelqrw.core.api.filter
+package com.immanuelqrw.core.api.test.unit.filter
 
+import com.immanuelqrw.core.api.filter.SearchCriterion
+import com.immanuelqrw.core.api.filter.SearchSpecification
 import com.immanuelqrw.core.api.test.Testable
 import com.immanuelqrw.core.api.model.BaseEntity
+import org.amshove.kluent.shouldBeNull
+import org.amshove.kluent.shouldEqual
+import org.amshove.kluent.shouldThrow
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.mockito.Mockito.`when`
+import javax.persistence.criteria.*
 
 /**
  * Unit tests for SearchSpecification
  */
 abstract class SearchSpecificationTest<T : BaseEntity> : Testable {
 
-    protected abstract val searchSpecification: SearchSpecification<T>
-    // TODO Mock root, criteriaQuery, builder
+    protected abstract val root: Root<T>
+    protected abstract val query: CriteriaQuery<*>
+    protected abstract val builder: CriteriaBuilder
 
-    // TODO Setup input data for test use cases
+    protected abstract val key: Path<String>
+
+    protected abstract val validPredicate: Predicate
+
+    protected abstract val validGreaterThanSearchCriterion: SearchCriterion
+    protected abstract val validLessThanSearchCriterion: SearchCriterion
+    protected abstract val validEqualitySearchCriterion: SearchCriterion
+    protected abstract val validLikeSearchCriterion: SearchCriterion
+
+    protected abstract val invalidKeySearchCriterion: SearchCriterion
+    protected abstract val invalidOperationSearchCriterion: SearchCriterion
 
     @BeforeAll
     override fun prepare() {}
@@ -23,20 +41,79 @@ abstract class SearchSpecificationTest<T : BaseEntity> : Testable {
     @BeforeEach
     override fun setUp() {}
 
-    // TODO Find out how to test Predicate
     @Nested
     inner class Success {
         @Test
-        fun `given valid search criterion - when values processed - returns Predicate`() {
-            assert(false)
+        fun `given valid greater than search criterion - when values processed - returns Predicate`() {
+            `when`(root.get<String>(validGreaterThanSearchCriterion.key)).thenReturn(key)
+            `when`(builder.greaterThanOrEqualTo(key, validGreaterThanSearchCriterion.value.toString())).thenReturn(validPredicate)
+
+            val searchSpecification: SearchSpecification<T> = SearchSpecification(validGreaterThanSearchCriterion)
+
+            val predicate: Predicate? = searchSpecification.toPredicate(root, query, builder)
+
+            predicate shouldEqual validPredicate
+        }
+
+        @Test
+        fun `given valid less than search criterion - when values processed - returns Predicate`() {
+            `when`(root.get<String>(validLessThanSearchCriterion.key)).thenReturn(key)
+            `when`(builder.lessThanOrEqualTo(key, validLessThanSearchCriterion.value.toString())).thenReturn(validPredicate)
+
+            val searchSpecification: SearchSpecification<T> = SearchSpecification(validLessThanSearchCriterion)
+
+            val predicate: Predicate? = searchSpecification.toPredicate(root, query, builder)
+
+            predicate shouldEqual validPredicate
+        }
+
+        @Test
+        fun `given valid equality search criterion - when values processed - returns Predicate`() {
+            `when`(root.get<String>(validEqualitySearchCriterion.key)).thenReturn(key)
+            `when`(builder.equal(key, validEqualitySearchCriterion.value)).thenReturn(validPredicate)
+
+            val searchSpecification: SearchSpecification<T> = SearchSpecification(validEqualitySearchCriterion)
+
+            val predicate: Predicate? = searchSpecification.toPredicate(root, query, builder)
+
+            predicate shouldEqual validPredicate
+        }
+
+        @Test
+        fun `given valid like search criterion - when values processed - returns Predicate`() {
+            `when`(root.get<String>(validLikeSearchCriterion.key)).thenReturn(key)
+            `when`(builder.like(key, validLikeSearchCriterion.value.toString())).thenReturn(validPredicate)
+
+            val searchSpecification: SearchSpecification<T> = SearchSpecification(validLikeSearchCriterion)
+
+            val predicate: Predicate? = searchSpecification.toPredicate(root, query, builder)
+
+            predicate shouldEqual validPredicate
         }
     }
 
     @Nested
     inner class Failure {
         @Test
-        fun `given invalid search criterion - when values processed - throws RuntimeException`() {
-            assert(false)
+        fun `given invalid search key - when values processed - throws IllegalArgumentException`() {
+            `when`(root.get<String>(invalidKeySearchCriterion.key)).thenThrow(IllegalArgumentException::class.java)
+
+            val searchSpecificationFunction = {SearchSpecification<T>(invalidKeySearchCriterion)}
+
+            // FIXME Test not throwing exception
+            searchSpecificationFunction.shouldThrow(IllegalArgumentException::class)
+        }
+
+        @Test
+        fun `given invalid search operation - when values processed - returns null`() {
+            `when`(root.get<String>(invalidOperationSearchCriterion.key)).thenReturn(key)
+            `when`(builder.equal(key, invalidOperationSearchCriterion.value)).thenReturn(validPredicate)
+
+            val searchSpecification: SearchSpecification<T> = SearchSpecification(invalidOperationSearchCriterion)
+
+            val predicate: Predicate? = searchSpecification.toPredicate(root, query, builder)
+
+            predicate.shouldBeNull()
         }
     }
 }
--- src/test/kotlin/com/immanuelqrw/core/api/filter/SolidSearchSpecificationTest.kt
+++ src/test/kotlin/com/immanuelqrw/core/api/filter/SolidSearchSpecificationTest.kt
@@ -1,11 +1,12 @@
 package com.immanuelqrw.core.api.filter
 
 import com.immanuelqrw.core.api.test.example.Solid
-import com.immanuelqrw.core.api.test.example.SolidSearchSpecification
+import com.immanuelqrw.core.api.test.unit.filter.SearchSpecificationTest
 import org.junit.jupiter.api.*
 import org.junit.jupiter.api.extension.ExtendWith
-import org.mockito.InjectMocks
+import org.mockito.Mock
 import org.springframework.test.context.junit.jupiter.SpringExtension
+import javax.persistence.criteria.*
 
 /**
  * Unit tests for SolidSearchSpecification
@@ -14,6 +15,26 @@ import org.springframework.test.context.junit.jupiter.SpringExtension
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 internal class SolidSearchSpecificationTest: SearchSpecificationTest<Solid>() {
 
-    @InjectMocks
-    override lateinit var searchSpecification: SolidSearchSpecification
+    @Mock
+    override lateinit var root: Root<Solid>
+
+    @Mock
+    override lateinit var query: CriteriaQuery<*>
+
+    @Mock
+    override lateinit var builder: CriteriaBuilder
+
+    @Mock
+    override lateinit var key: Path<String>
+
+    @Mock
+    override lateinit var validPredicate: Predicate
+
+    override val validGreaterThanSearchCriterion = SearchCriterion("id", ">", 2)
+    override val validLessThanSearchCriterion = SearchCriterion("id", "<", 2)
+    override val validEqualitySearchCriterion = SearchCriterion("id", ":", 2)
+    override val validLikeSearchCriterion = SearchCriterion("id", "~", 2)
+
+    override val invalidKeySearchCriterion = SearchCriterion("ig", ":", 2)
+    override val invalidOperationSearchCriterion = SearchCriterion("id", "^", 2)
 }
