d6b1503b478cb6c2777566037ca6cda6e597976b
Immanuel Washington <code.immanuelqrw@gmail.com>
2019-03-28 01:26:43 -0400

    Controller Unit Tests outline
    - Created outline for unit tests
    - Non-functional as need to find errors
    - Non-functional as need to figure out output checking

--- src/main/kotlin/com/immanuelqrw/core/api/test/unit/controller/BaseControllerTest.kt
+++ src/main/kotlin/com/immanuelqrw/core/api/test/unit/controller/BaseControllerTest.kt
@@ -4,35 +4,59 @@ import com.immanuelqrw.core.api.controller.BaseController
 import com.immanuelqrw.core.api.model.BaseEntity
 import com.immanuelqrw.core.api.service.BaseService
 import com.immanuelqrw.core.api.test.Testable
-import org.junit.jupiter.api.BeforeAll
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Nested
-import org.junit.jupiter.api.Test
+import com.immanuelqrw.core.api.utility.Utility.OBJECT_MAPPER
+import org.amshove.kluent.shouldEqual
+import org.junit.jupiter.api.*
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.doNothing
+import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.MvcResult
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import javax.persistence.EntityNotFoundException
 
 /**
  * Unit tests for Controller
  */
 abstract class BaseControllerTest<T : BaseEntity> : Testable {
 
+    protected abstract val mvc: MockMvc
+
     protected abstract val controller: BaseController<T>
 
     protected abstract val service: BaseService<T>
 
-    // TODO Setup input data for test use cases
+    protected abstract val entityName: String
 
     protected abstract val validId: Long
     protected abstract val invalidId: Long
+
     protected abstract val validEntity: T
     protected abstract val invalidEntity: T
-    protected abstract val validPage: Pageable
-    protected abstract val invalidPage: Pageable
+    protected abstract val replacedEntity: T
+
+    protected abstract val validPatchedFields: Map<String, Any>
+    protected abstract val invalidPatchedFields: Map<String, Any>
+
+    protected abstract val validPageable: Pageable
+    protected abstract val invalidPageable: Pageable
+    protected abstract val validPage: Page<T>
+
     protected abstract val validSearch: String
     protected abstract val invalidSearch: String
+
+    private lateinit var baseUri: String
+    private lateinit var idUri: String
 
     @BeforeAll
     override fun prepare() {
         // Subclass implementation
+
+        baseUri = "/$entityName"
+        idUri = "$baseUri/{id}"
     }
 
     @BeforeEach
@@ -40,41 +64,121 @@ abstract class BaseControllerTest<T : BaseEntity> : Testable {
         // Subclass implementation
     }
 
+
+    // FIXME Replace /solid with /{entityName} and make overriden variable
     @Nested
     inner class Success {
+        // FIXME Deserialize mock output
         @Test
         fun `given valid id - when GET entity - returns entity`() {
-            assert(false)
+            `when`(service.find(validId)).thenReturn(validEntity)
+
+            val mvcResult: MvcResult = mvc.perform(
+                get(idUri, validId)
+                    .contentType(MediaType.APPLICATION_JSON)
+            )
+                .andExpect(status().isOk)
+                .andReturn()
+
+            // FIXME
+            mvcResult.response
+
+            // val actualEntity: T = OBJECT_MAPPER.readValue(mvcResult.response.contentAsString, T::class.java)
+            // actualEntity.id shouldEqual validEntity.id
         }
 
         @Test
         fun `given valid page, sort, and search parameters - when GET entities - returns entities`() {
-            assert(false)
+            `when`(service.findAll(validPageable, validSearch)).thenReturn(validPage)
+
+            val mvcResult: MvcResult = mvc.perform(
+                get(baseUri)
+                    .contentType(MediaType.APPLICATION_JSON)
+            )
+                .andExpect(status().isOk)
+                .andReturn()
+
+            // FIXME
+            mvcResult.response
         }
 
         @Test
         fun `given valid entity - when POST entity - persists entity`() {
-            assert(false)
+            `when`(service.create(validEntity)).thenReturn(validEntity)
+
+            val mvcResult: MvcResult = mvc.perform(
+                post(baseUri)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(OBJECT_MAPPER.writeValueAsBytes(validEntity))
+            )
+                .andExpect(status().isNoContent)
+                .andReturn()
+
+            // FIXME
+            mvcResult.response
         }
 
         @Test
         fun `given valid id and entity to replace - when PUT entity - replaces entity`() {
-            assert(false)
+            `when`(service.replace(validId, validEntity)).thenReturn(replacedEntity)
+
+            val mvcResult: MvcResult = mvc.perform(
+                put(idUri, validId)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(OBJECT_MAPPER.writeValueAsBytes(validEntity))
+            )
+                .andExpect(status().isNoContent)
+                .andReturn()
+
+            // FIXME
+            mvcResult.response
         }
 
         @Test
         fun `given valid id and fields to replace - when PATCH entity - modifies entity`() {
-            assert(false)
+            `when`(service.modify(validId, validPatchedFields)).thenReturn(replacedEntity)
+
+            val mvcResult: MvcResult = mvc.perform(
+                patch(idUri, validId)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(OBJECT_MAPPER.writeValueAsBytes(validPatchedFields))
+            )
+                .andExpect(status().isNoContent)
+                .andReturn()
+
+            // FIXME
+            mvcResult.response
         }
 
         @Test
         fun `given valid id - when DELETE entity - sets entity removedOn to now`() {
-            assert(false)
+            doNothing().`when`(service).remove(validId)
+
+            val mvcResult: MvcResult = mvc.perform(
+                delete(idUri, validId)
+                    .contentType(MediaType.APPLICATION_JSON)
+            )
+                .andExpect(status().isOk)
+                .andReturn()
+
+            // FIXME
+            mvcResult.response
         }
 
         @Test
         fun `given valid page, sort, and search parameters - when DELETE entities - sets entities' removedOn to now`() {
-            assert(false)
+            doNothing().`when`(service).removeAll(validPageable, validSearch)
+
+            val mvcResult: MvcResult = mvc.perform(
+                delete(baseUri)
+                    .contentType(MediaType.APPLICATION_JSON)
+            )
+                .andExpect(status().isOk)
+                .andReturn()
+
+            // FIXME
+            mvcResult.response
+            // FIXME Add rest of logic for iterating through page
         }
     }
 
@@ -84,40 +188,88 @@ abstract class BaseControllerTest<T : BaseEntity> : Testable {
         inner class InvalidUriPath {
             @Test
             fun `given invalid id - when GET entity - returns NotFound response`() {
-                assert(false)
+                `when`(service.find(invalidId)).thenThrow(EntityNotFoundException::class.java)
+
+                mvc.perform(
+                    get(idUri, invalidId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                    .andExpect(status().isNotFound)
             }
 
             @Test
             fun `given invalid id - when PUT entity - returns NotFound response`() {
-                assert(false)
+                `when`(service.replace(invalidId, validEntity)).thenThrow(EntityNotFoundException::class.java)
+
+                mvc.perform(
+                    put(idUri, invalidId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(OBJECT_MAPPER.writeValueAsBytes(validEntity))
+                )
+                    .andExpect(status().isNotFound)
             }
 
             @Test
             fun `given invalid id - when PATCH entity - returns NotFound response`() {
-                assert(false)
+                `when`(service.modify(invalidId, validPatchedFields)).thenThrow(EntityNotFoundException::class.java)
+
+                mvc.perform(
+                    patch(idUri, invalidId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(OBJECT_MAPPER.writeValueAsBytes(validPatchedFields))
+                )
+                    .andExpect(status().isNotFound)
             }
 
             @Test
             fun `given invalid id - when DELETE entity - returns NotFound response`() {
-                assert(false)
+                `when`(service.remove(invalidId)).thenThrow(EntityNotFoundException::class.java)
+
+                mvc.perform(
+                    delete(idUri, invalidId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                    .andExpect(status().isNotFound)
             }
         }
 
+        // FIXME Find which error thrown by bad entity
         @Nested
         inner class InvalidBody {
             @Test
             fun `given invalid entity - when POST entity - returns BadRequest response`() {
-                assert(false)
+                `when`(service.create(invalidEntity)).thenThrow()
+
+                mvc.perform(
+                    post(baseUri)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(OBJECT_MAPPER.writeValueAsBytes(invalidEntity))
+                )
+                    .andExpect(status().isBadRequest)
             }
 
             @Test
             fun `given invalid entity - when PUT entity - returns BadRequest response`() {
-                assert(false)
+                `when`(service.replace(validId, invalidEntity)).thenThrow()
+
+                mvc.perform(
+                    put(baseUri)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(OBJECT_MAPPER.writeValueAsBytes(invalidEntity))
+                )
+                    .andExpect(status().isBadRequest)
             }
 
             @Test
             fun `given invalid partial entity - when PATCH entity - returns BadRequest response`() {
-                assert(false)
+                `when`(service.modify(validId, invalidPatchedFields)).thenThrow()
+
+                mvc.perform(
+                    put(baseUri)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(OBJECT_MAPPER.writeValueAsBytes(invalidPatchedFields))
+                )
+                    .andExpect(status().isBadRequest)
             }
         }
 
@@ -125,32 +277,70 @@ abstract class BaseControllerTest<T : BaseEntity> : Testable {
         inner class InvalidQueryParam {
             @Test
             fun `given invalid page parameter - when GET entities - returns BadRequest response`() {
-                assert(false)
+                `when`(service.findAll(invalidPageable, validSearch)).thenThrow()
+
+                mvc.perform(
+                    get(baseUri)
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                    .andExpect(status().isBadRequest)
             }
 
+            // FIXME Look into if there is a way to make sort parameter fail separately
             @Test
             fun `given invalid sort parameter - when GET entities - returns BadRequest response`() {
-                assert(false)
+                `when`(service.findAll(invalidPageable, validSearch)).thenThrow()
+
+                mvc.perform(
+                    get(baseUri)
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                    .andExpect(status().isBadRequest)
             }
 
             @Test
             fun `given invalid search parameters - when GET entities - returns BadRequest response`() {
-                assert(false)
+                `when`(service.findAll(validPageable, invalidSearch)).thenThrow()
+
+                mvc.perform(
+                    get(baseUri)
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                    .andExpect(status().isBadRequest)
             }
 
             @Test
             fun `given invalid page parameter - when DELETE entities - returns BadRequest response`() {
-                assert(false)
+                `when`(service.findAll(invalidPageable, validSearch)).thenThrow()
+
+                mvc.perform(
+                    delete(baseUri)
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                    .andExpect(status().isBadRequest)
             }
 
+            // FIXME Look into if there is a way to make sort parameter fail separately
             @Test
             fun `given invalid sort parameter - when DELETE entities - returns BadRequest response`() {
-                assert(false)
+                `when`(service.findAll(invalidPageable, validSearch)).thenThrow()
+
+                mvc.perform(
+                    delete(baseUri)
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                    .andExpect(status().isBadRequest)
             }
 
             @Test
             fun `given invalid search parameters - when DELETE entities - returns BadRequest response`() {
-                assert(false)
+                `when`(service.removeAll(validPageable, invalidSearch)).thenThrow()
+
+                mvc.perform(
+                    delete(baseUri)
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                    .andExpect(status().isBadRequest)
             }
         }
     }
--- src/test/kotlin/com/immanuelqrw/core/api/controller/SolidControllerTest.kt
+++ src/test/kotlin/com/immanuelqrw/core/api/controller/SolidControllerTest.kt
@@ -7,10 +7,15 @@ import com.immanuelqrw.core.api.test.unit.controller.BaseControllerTest
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Controller
 import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.test.web.servlet.MockMvc
 
 /**
  * Unit tests for SolidController
@@ -20,9 +25,44 @@ import org.springframework.test.context.junit.jupiter.SpringExtension
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 internal class SolidControllerTest : BaseControllerTest<Solid>() {
 
+    @Autowired
+    override lateinit var mvc: MockMvc
+
     @InjectMocks
     override lateinit var controller: SolidController
 
     @MockBean
     override lateinit var service: SolidService
+
+    override val validId: Long = 1
+    override val invalidId: Long = -1
+
+    @Mock
+    override lateinit var validEntity: Solid
+
+    @Mock
+    override lateinit var invalidEntity: Solid
+
+    @Mock
+    override lateinit var replacedEntity: Solid
+
+    @Mock
+    override lateinit var validPatchedFields: Map<String, Any>
+
+    @Mock
+    override lateinit var invalidPatchedFields: Map<String, Any>
+
+    @Mock
+    override lateinit var validPageable: Pageable
+
+    @Mock
+    override lateinit var invalidPageable: Pageable
+
+    @Mock
+    override lateinit var validPage: Page<Solid>
+
+    override val validSearch: String = "id:2"
+    override val invalidSearch: String = "id@2"
+
+    override val entityName: String = "solid"
 }
